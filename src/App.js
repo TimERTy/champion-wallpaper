a050a154061746ec6094a2ec8847fae267032c61
TimERTy <tmahonert@gmail.com>
2019-06-09 23:41:49 +1000

    Input handling and some api fetch, still need to get around CORS

--- src/App.js
+++ src/App.js
@@ -6,13 +6,15 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
 
     constructor(){
-     super()
-     this.state={
-        text: ""
-      }
-      this.getChampion = this.getChampion.bind(this);
+        super()
+        this.state={
+            text: "Summoner Name",
+            api_key: "RGAPI-a9e48566-c1f4-46ab-97ec-614a75b37691",
+            summonerName: "TimERTy"
+        }
+        this.getChampion = this.getChampion.bind(this);
     }
-    
+
     fetchJson(url) {
         fetch(url)
         .then(response => response.json())
@@ -29,22 +31,16 @@ class App extends React.Component {
     
     getAccountId(summonerName) {
         //Gets the api based off of the summoner name provided
-        let url = "https://oc1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+summonerName+"?api_key=RGAPI-4691fa9f-f6fd-4a58-9d55-69050f28aaad";
-        //var promise = new Promise(fetchJson(url))
-        //.then((data) => {
-        //    return data.accountId;
-        //});
+        let url = "https://oc1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + this.summonerName + "?api_key=" + this.api_key;
+        console.log (this.fetchJson (url));
+        //
     }
     
-    getChampion(e) {
+    getChampion() {
         //League of Legends Api
         //  This function uses lol dev api to figure out the last played champion played by the user
         //  The User will input thier IGN (in game name) which will then trigger an api search
-        console.log (e);
-
-        let api_key = "RGAPI-4691fa9f-f6fd-4a58-9d55-69050f28aaad";
-        let summonerName = "TimERTy";
-        this.getAccountId(summonerName);
+        this.getAccountId(this.state.text);
     }
 
     render() {
@@ -64,10 +60,19 @@ class App extends React.Component {
             className="App-input"
             type="text"
             id="text"
-            onKeyPress={(ev)=>{
-              if(ev.key == "Enter"){
+            value={this.state.text}
+            onChange={(e) => {
+              let regex = new RegExp ("^[0-9a-zA-Z _.]+$");
+              if (regex.test(e.key)) {
+                  this.setState({text:e.target.value});
+              } else {
+                  console.log("bad");
+              }
+            }}
+            onKeyDown={(e)=>{
+              if(e.key == "Enter"){
                 console.log("submit something?");
-                this.getChampion(document.getElementById("text").value);
+                this.getChampion();
               }
             }}
         />
