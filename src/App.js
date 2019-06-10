b4572a1936e390d9ae3ed753b2bd335a78ffdff8
TimERTy <tmahonert@gmail.com>
2019-06-10 20:24:27 +1000

    Prettier used

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-
-    constructor(){
-        super()
-        this.state={
-            text: "Summoner Name",
-            api_key: "RGAPI-a9e48566-c1f4-46ab-97ec-614a75b37691",
-            summonerName: "TimERTy"
-        }
+    constructor() {
+        super();
+        this.state = {
+            text: 'Summoner Name',
+            api_key: 'RGAPI-a9e48566-c1f4-46ab-97ec-614a75b37691',
+            summonerName: 'TimERTy',
+        };
         this.getChampion = this.getChampion.bind(this);
     }
 
     fetchJson(url) {
         fetch(url)
-        .then(response => response.json())
-        .then((jsonData) => {
-            // jsonData is parsed json object received from url
-            console.log(jsonData);
-            return jsonData;
-        })
-        .catch((error) => {
-            console.error(error);
-        });
+            .then(response => response.json())
+            .then(jsonData => {
+                // jsonData is parsed json object received from url
+                console.log(jsonData);
+                return jsonData;
+            })
+            .catch(error => {
+                console.error(error);
+            });
         return false;
     }
-    
+
     getAccountId(summonerName) {
         //Gets the api based off of the summoner name provided
-        let url = "https://oc1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + this.summonerName + "?api_key=" + this.api_key;
-        console.log (this.fetchJson (url));
+        let url = 'https://oc1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + this.summonerName + '?api_key=' + this.api_key;
+        console.log(this.fetchJson(url));
         //
     }
-    
+
     getChampion() {
         //League of Legends Api
         //  This function uses lol dev api to figure out the last played champion played by the user
@@ -44,41 +43,36 @@ class App extends React.Component {
     }
 
     render() {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-        <input 
-            className="App-input"
-            type="text"
-            id="text"
-            value={this.state.text}
-            onChange={(e) => {
-              let regex = new RegExp ("^[0-9a-zA-Z _.]+$");
-              if (regex.test(e.key)) {
-                  this.setState({text:e.target.value});
-              } else {
-                  console.log("bad");
-              }
-            }}
-            onKeyDown={(e)=>{
-              if(e.key == "Enter"){
-                console.log("submit something?");
-                this.getChampion();
-              }
-            }}
-        />
-          </header>
-        </div>
-      );
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                        Learn React
+                    </a>
+                    <input
+                        className="App-input"
+                        type="text"
+                        id="text"
+                        value={this.state.text}
+                        onChange={e => {
+                            let regex = new RegExp('^[0-9a-zA-Z _.]+$');
+                            if (regex.test(e.key)) {
+                                this.setState({text: e.target.value});
+                            } else {
+                                console.log('bad');
+                            }
+                        }}
+                        onKeyDown={e => {
+                            if (e.key === 'Enter') {
+                                console.log('submit something?');
+                                this.getChampion();
+                            }
+                        }}
+                    />
+                </header>
+            </div>
+        );
     }
 }
 
