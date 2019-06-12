ad3910156cb9ec2783de03281e6c1e7a75896041
TimERTy <tmahonert@gmail.com>
2019-06-12 15:54:34 +1000

    Pulls the last 10 wins/losses

--- src/App.js
+++ src/App.js
@@ -10,18 +10,18 @@ class App extends React.Component {
             summonerName: "",
             accountId: "",
             matches: [],
+            matchesCollection: {},
             champName: "",
             champNum: 0,
-            getChampion: 0
+            getChampion: 0,
+            showMatches: 0
         };
     }
 
     getAccountId() {
         //Gets the api based off of the summoner name provided
         fetch("/api/accountId/" + this.state.summonerName)
-            .then(res => {
-                return res.json();
-            })
+            .then(res => res.json())
             .then(data => this.setState({ accountId: data.accountId }));
     }
 
@@ -29,9 +29,38 @@ class App extends React.Component {
         //this function will
         fetch("/api/matchHistory/" + this.state.accountId)
             .then(res => res.json())
-            .then(data =>
-                this.setState({ matches: data, champNum: data[0].champion })
-            );
+            .then(data => this.setState({ matches: data, champNum: data[0].champion }));
+    }
+
+    getMatch(matchId) {
+        //this function will
+        fetch("/api/match/" + matchId)
+            .then(res => res.json())
+            .then(data => {
+                let team = -1;
+                for (let identity = 0; identity < 10; identity++) {
+                    if (data.participantIdentities[identity].player.summonerName === this.state.summonerName) {
+                        team = Math.floor((data.participantIdentities[identity].participantId - 1) / 5);
+                        //console.log("Participant ID: ", data.participantIdentities[identity].participantId);
+                    }
+                }
+                //console.log(team);
+                this.setState(prevState => ({
+                    matchesCollection: {
+                        ...prevState.matchesCollection,
+                        [matchId]: data.teams[team].win
+                    }
+                }));
+            });
+    }
+
+    getMatches() {
+        //console.log(Object.keys(this.state.matches));
+        for (let index = 0; index < 10; index++) {
+            this.getMatch(this.state.matches[index].gameId);
+        }
+        //console.log(matchesCollection);
+        //setTimeout(() => console.log(matchesCollection), 3000);
     }
 
     getChampName() {
@@ -55,6 +84,8 @@ class App extends React.Component {
                 this.getChampName();
             } else if (prevState.matches !== this.state.matches) {
                 //TODO open display match data nicely
+            } else if (prevState.showMatches !== this.state.showMatches) {
+                if (this.state.showMatches === 1) this.getMatches();
             }
         }
     }
@@ -77,7 +108,6 @@ class App extends React.Component {
                                 //  This function uses lol dev api to figure out the last played champion played by the user
                                 //  The User will input thier IGN (in game name) which will then trigger an api search
                                 this.setState({
-                                    getChampion: 1,
                                     text: e.target.value
                                 });
                             } else {
@@ -87,33 +117,35 @@ class App extends React.Component {
                         onKeyDown={e => {
                             if (e.key === "Enter") {
                                 this.setState({
+                                    getChampion: 1,
                                     summonerName: this.state.text
                                 });
                             }
                         }}
                     />
-                    <div className="App-details">
-                        <p>
-                            {this.state.accountId
-                                ? this.state.accountId
-                                : "No Account ID"}
-                        </p>
-                        <p>
-                            {this.state.summonerName
-                                ? this.state.summonerName
-                                : "No Summoner Name"}
-                        </p>
-                        <p>
-                            {this.state.matches[0]
-                                ? this.state.matches[0].champion
-                                : "No Matches"}
-                        </p>
-                        <p>
-                            {this.state.champName
-                                ? this.state.champName
-                                : "No Champion Name"}
-                        </p>
-                    </div>
+                    <button
+                        className="App-matches-button"
+                        onClick={e => {
+                            this.setState({ showMatches: this.state.showMatches ^ 1 });
+                        }}
+                    >
+                        Button
+                    </button>
+                    {!this.state.showMatches ? (
+                        <div className="App-details">
+                            <p>{this.state.accountId ? this.state.accountId : "No Account ID"}</p>
+                            <p>{this.state.summonerName ? this.state.summonerName : "No Summoner Name"}</p>
+                            <p>{this.state.matches[0] ? this.state.matches[0].champion : "No Matches"}</p>
+                            <p>{this.state.champName ? this.state.champName : "No Champion Name"}</p>
+                        </div>
+                    ) : (
+                        <div className="App-matches">
+                            {/* display matches */}
+                            {Object.keys(this.state.matchesCollection).map(item => {
+                                return <div>{this.state.matchesCollection[item]}</div>;
+                            })}
+                        </div>
+                    )}
                 </header>
             </div>
         );
